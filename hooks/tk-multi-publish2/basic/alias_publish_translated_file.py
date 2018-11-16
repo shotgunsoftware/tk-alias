7e8795ef87b1298a226deb70812954378163b840
Miguel Tapia <miguel.tapia@autodesk.com>
2018-11-16 13:29:03 -0600

    [SHOT-2713] Use wire thumbnail for non wire files when publishing using ALIAS (#16)
    [SHOT-2695] Change name to  "Import into New Stage"

--- hooks/tk-multi-publish2/basic/alias_publish_translated_file.py
+++ hooks/tk-multi-publish2/basic/alias_publish_translated_file.py
@@ -232,6 +232,8 @@ class AliasPublishTranslatedFilePlugin(HookBaseClass):
                 return publish_type
 
     def publish(self, settings, item):
+        publish_id = item.properties['sg_publish_data']['id']
+        
         item.properties["publish_template"] = item.properties[self.publish_template_key]
         item.properties["work_template"] = item.properties[self.work_template_key]
 
@@ -239,6 +241,18 @@ class AliasPublishTranslatedFilePlugin(HookBaseClass):
         item.local_properties.publish_type = publish_type
 
         super(AliasPublishTranslatedFilePlugin, self).publish(settings, item)
+        
+        entities = [
+            {
+                'type': 'PublishedFile',
+                'id': item.properties['sg_publish_data']['id']
+            }
+        ]
+        source_entity = {
+            'type': 'PublishedFile',
+            'id': publish_id
+        }
+        self.parent.engine.shotgun.share_thumbnail(entities=entities, source_entity=source_entity)
 
     @property
     def item_filters(self):
