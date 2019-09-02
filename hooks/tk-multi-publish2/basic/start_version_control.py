3075453ce630ff7329e0e172f751938603f7030f
Barbara Laigneau <barbara@dark-shot.com>
2019-09-02 11:29:35 +0200

    add new function to display a Save As dialog as Alias api doesn't provided one

---
+++ hooks/tk-multi-publish2/basic/start_version_control.py
@@ -0,0 +1,7 @@
+"""
+Copyright (c) 2019 Dark Shot
+
+CONFIDENTIAL AND PROPRIETARY
+
+:author: Barbara Laigneau <barbara@dark-shot.com>
+"""
--- python/tk_alias/operations.py
+++ python/tk_alias/operations.py
@@ -159,6 +159,34 @@ class AliasOperations(object):
 
         self._engine.execute_hook_method("file_usage_hook", "file_attempt_open", path=path)
 
+    def open_save_as_dialog(self):
+        """
+        Launch a Qt file browser to select a file, then save the supplied
+        project to that path.
+        """
+
+        # Alias doesn't appear to have a "save as" dialog accessible via
+        # python. so open our own Qt file dialog.
+        file_dialog = QtGui.QFileDialog(
+            parent=self.get_parent_window(),
+            caption="Save As",
+            directory=os.path.expanduser("~"),
+            filter="Alias file (*.wire)"
+        )
+        file_dialog.setLabelText(QtGui.QFileDialog.Accept, "Save")
+        file_dialog.setLabelText(QtGui.QFileDialog.Reject, "Cancel")
+        file_dialog.setOption(QtGui.QFileDialog.DontResolveSymlinks)
+        file_dialog.setOption(QtGui.QFileDialog.DontUseNativeDialog)
+        if not file_dialog.exec_():
+            return
+        path = file_dialog.selectedFiles()[0]
+
+        if os.path.splitext(path)[-1] != ".wire":
+            path = "{0}.wire".format(path)
+
+        if path:
+            self.save_file_as(path)
+
     def reset_scene(self):
         """Reset the current scene."""
         self.logger.debug("Resetting the scene")
