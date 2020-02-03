ef7f00bf05f4e6e90fe13f92a12a0ebe2962825e
Barbara Laigneau <barbara@dark-shot.com>
2020-02-03 18:39:55 +0100

    remove trailing whitespaces and extra blank lines

--- hooks/tk-multi-publish2/basic/publish_annotations.py
+++ hooks/tk-multi-publish2/basic/publish_annotations.py
@@ -29,12 +29,12 @@ class PublishAnnotationsPlugin(HookBaseClass):
     def description(self):
         return """
         <p>
-            This plugin exports all annotations created using the Locator Annotation tool in Alias. 
+            This plugin exports all annotations created using the Locator Annotation tool in Alias.
         </p>
         <p>
             Each annotation will create a Note in Shotgun. All Notes are linked to this version and file. Use this to 
             sync all review notes made in Alias with Shotgun.
-        </p> 
+        </p>
         """
 
     @property
@@ -159,5 +159,3 @@ class PublishAnnotationsPlugin(HookBaseClass):
         :param item: Item to process
         """
         self.logger.info("Annotations published successfully")
-
-
