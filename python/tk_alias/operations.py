9a1a97d946c98c91e6d607bf16a0580beb87c090
Ariel Calzada <ariel.calzada.solano@autodesk.com>
2020-03-18 09:29:34 -0500

    Fixed typo

--- python/tk_alias/operations.py
+++ python/tk_alias/operations.py
@@ -460,7 +460,7 @@ class AliasOperations(object):
         uses the source path to get the folder and returns it.
 
         source_path: It's a filesystem path. The file name has the form {name}.{version}.{extension}
-                     Ex. C:\myproject\assets\Vehicle\myasset\CSA\publish\alias\scene.v002.wire
+                     Ex. r"C:\myproject\assets\Vehicle\myasset\CSA\publish\alias\scene.v002.wire"
         """
         # get the template object defined in the info.yml
         reference_template = self._engine.get_template("reference_template")
