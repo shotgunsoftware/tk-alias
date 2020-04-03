a699325c22287ef0752f243f9cd2068e0769fab5
Barbara Laigneau <barbara@dark-shot.com>
2020-04-03 16:11:31 +0200

    fix Python3 syntax

--- engine.py
+++ engine.py
@@ -304,7 +304,7 @@ class AliasEngine(sgtk.platform.Engine):
             else:
                 if not setting_command_name:
                     # Run all commands of the given app instance.
-                    for (command_name, command_function) in command_dict.iteritems():
+                    for (command_name, command_function) in command_dict.items():
                         self.logger.debug(
                             "%s startup running app '%s' command '%s'.",
                             self.name,
