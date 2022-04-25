42c0ba6153bd6a01e7b7cb883a044456daf62136
Stacey Oue <stacey.oue@autodesk.com>
2022-04-25 16:16:47 -0400

    Remove unused hook methods. Documentation.

--- hooks/tk-multi-data-validation/basic/scene_data_validator.py
+++ hooks/tk-multi-data-validation/basic/scene_data_validator.py
@@ -20,51 +20,18 @@ class SceneDataValidatorHook(HookBaseClass):
 
     def get_validation_data(self):
         """
-        Return the scene action data.
+        Return the validation rule data set to validate an Alias scene.
 
-        The dictionary returned by this function should be formated such that it can be passed to the
-        ValidationRule class constructor to ValidationRule objects.
+        This method will retrieve the default validation rules returned by
+        :meth:`AliasSceneDataValidator.get_validation_data`. To customize the default
+        validation rules, override this hook method to modify the returned data dictionary.
 
-        :return: The scene validation rules data to be consumed by the ValidationRule class constructor.
+        The dictionary returned by this function should be formated such that it can be passed
+        to the :class:`~tk-multi-data-validation:data.ValidationRule` class constructor to
+        create a new validation rule object.
+
+        :return: The validation rules data set.
         :rtype: dict
         """
 
         return self.parent.engine.scene_data_validator.get_validation_data()
-
-    def execute_check_action(self, action_name, *args, **kwargs):
-        """
-        Execute the Alias scene validation check function.
-
-        :param action_name: The unique id to get the check function to execute.
-        :type action_name: str
-        :param args: The arguments list to pass to the check function.
-        :type args: list
-        :param kwargs: The keyword arguments dict to pass to the check function.
-        :type kwargs: dict
-
-        :return: The result of the check function that was executed.
-        :rtype: AliasSceneDataValidator.CheckResult
-        """
-
-        return self.parent.engine.scene_data_validator.execute_check_action(
-            action_name, *args, **kwargs
-        )
-
-    def execute_fix_action(self, action_name, *args, **kwargs):
-        """
-        Execute the Alias scene validation fix function.
-
-        :param action_name: The unique id to get the fix function to execute.
-        :type action_name: str
-        :param args: The arguments list to pass to the check function.
-        :type args: list
-        :param kwargs: The keyword arguments dict to pass to the check function.
-        :type kwargs: dict
-
-        :return: The result of the fix function that was executed.
-        :rtype: any
-        """
-
-        return self.parent.engine.scene_data_validator.execute_fix_action(
-            action_name, *args, **kwargs
-        )
