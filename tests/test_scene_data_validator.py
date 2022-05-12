3b2c39efa4ab386a980df36231be8f3e9fb58f78
Stacey Oue <stacey.oue@autodesk.com>
2022-05-12 11:00:06 -0400

    Add tests folder with initial set up.

---
+++ tests/test_scene_data_validator.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2020 Autodesk, Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Autodesk, Inc.
+
+import os
+import pytest
+
+# from app_test_base import AppTestBase
+from tank_test.tank_test_base import setUpModule  # noqa
+
+
+def test_check():
+    assert True
