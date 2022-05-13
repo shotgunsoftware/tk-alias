ae91431ebfb5300ca2cea30bbbb26a8088a58a04
Stacey Oue <stacey.oue@autodesk.com>
2022-05-13 16:03:57 -0400

    Add exception specific to importing the Alias Python API. Add method to compare version strings.

--- api/alias_api/__init__.py
+++ api/alias_api/__init__.py
@@ -26,6 +26,56 @@ ALIAS_API = {
 }
 
 
+class AliasPythonAPIImportError(Exception):
+    """Exception for Alias Python API import errors."""
+
+    pass
+
+
+def version_cmp(version1, version2):
+    """
+    Compare the version strings.
+
+    :param version1: A version string to compare against version2 e.g. 2022.2
+    :param version2: A version string to compare against version1 e.g. 2021.3.1
+
+    :return: The result of the comparison:
+         1 - version1 is greater than version2
+         0 - version1 and version2 are equal
+        -1 - version1 is less than version2
+    :rtype: ine
+    """
+
+    # This will split both the versions by the '.' char to get the major, minor, patch values
+    arr1 = version1.split(".")
+    arr2 = version2.split(".")
+    n = len(arr1)
+    m = len(arr2)
+
+    # Converts to integer from string
+    arr1 = [int(i) for i in arr1]
+    arr2 = [int(i) for i in arr2]
+
+    # Compares which list is bigger and fills the smaller list with zero (for unequal
+    # delimeters)
+    if n > m:
+        for i in range(m, n):
+            arr2.append(0)
+    elif m > n:
+        for i in range(n, m):
+            arr1.append(0)
+
+    # Returns 1 if version1 is greater
+    # Returns -1 if version2 is greater
+    # Returns 0 if they are equal
+    for i in range(len(arr1)):
+        if arr1[i] > arr2[i]:
+            return 1
+        elif arr2[i] > arr1[i]:
+            return -1
+    return 0
+
+
 def import_alias_api():
     """
     Import the right module according to some criteria:
